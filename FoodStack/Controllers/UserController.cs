b05e612e0b8cfa60310c5e9f12fdf48bcbb9965c
Vittorio Vaselli <vittorio.vaselli@outlook.com>
2020-04-30 11:25:44 +0200

    login or create user api

---
+++ FoodStack/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodStack.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodStack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationContext _context;
+
+
+        public UserController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        //create user or login
+        // POST api/user
+        [HttpPost]
+        public ActionResult Post([FromBody] User user)
+        {
+            User existingUser = _context
+                .Users
+                .FirstOrDefault
+                (
+                        u => (u.ProviderId == user.ProviderId) && (u.LoginProvider == user.LoginProvider)
+                );
+
+            if (existingUser!= null)
+            {
+                return StatusCode(200, user);
+            }
+            _context.Users.Add(user);
+
+            _context.SaveChanges();
+
+            return StatusCode(201, user);
+        }
+    }
+}
--- FoodStack/Models/User.cs
+++ FoodStack/Models/User.cs
@@ -8,9 +8,12 @@ namespace FoodStack.Models
     public class User
     {
         public Guid Id { get; set; }
-        public string SocialId { get; set; }
+        public string ProviderId { get; set; }
+        public LoginProvider LoginProvider { get; set; }
         public string  Name { get; set; }
         public string ImgUrl { get; set; }
         public string Email { get; set; }
     }
+
+    public enum LoginProvider { Google}
 }
